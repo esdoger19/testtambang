dc2423e26054878f2c65c3a7124baede2c90bf35
esdoger19 <84504126+esdoger19@users.noreply.github.com>
2021-05-24 11:23:23 +0700

    Create zcash.sh

---
+++ zcash.sh
@@ -0,0 +1,5 @@
+# zcash
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u t1TAknDqLBGwAG9SSD3ukSVgMb7wjk9TU4h.pandji -p x --cpu 4
